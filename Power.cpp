f17571c239237385b4acae827d69f72e535c5034
TITANsj <suryanshj8@gmail.com>
2022-01-25 23:26:20 +0530

    Power

---
+++ Power.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int power(int a, int n){
+    if(n==0)
+        return 1;
+    if(n==1)
+        return a;
+    return a*power(a,n-1);
+}
+
+int main() {
+    int a,n;
+    cin>>a>>n;
+    cout<<power(a,n);
+    return 0;
+}
