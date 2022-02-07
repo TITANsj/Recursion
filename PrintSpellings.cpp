302db0720ae98416ba42eb7562d8dfb5a70d8424
TITANsj <suryanshj8@gmail.com>
2022-02-08 04:55:39 +0530

    PrintSpellings

---
+++ PrintSpellings.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+char words[][10]={"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+void printSpellings(int n){
+    if(n==0)
+        return;
+    printSpellings(n/10);
+    int digit = n%10;
+    cout<<words[digit]<<" ";
+}
+
+int main() {
+    int n;
+    cin>>n;
+    printSpellings(n);
+    return 0;
+}
